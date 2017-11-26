663406fa27aede5268a2327c4d3db1dfaa224c5e
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-25 23:06:32 -0800

    Create action and gave argument to selectId

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,7 +4,7 @@ import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
-    const { titleStyle }  = styles
+    const { titleStyle } = styles;
     return (
     <CardSection>
       <Text style={titleStyle}>
