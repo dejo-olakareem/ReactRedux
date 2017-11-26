0b97c8caecfa2ff3e1e9916d3444941538d2bf70
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-25 20:19:54 -0800

    initialize life cycle method componentWillMount to get data source

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHAsChanged: (r1, r2) => r1 !== r2
+    });
+  this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
   render() {
     return;
   }
